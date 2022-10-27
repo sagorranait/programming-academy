d2a3d8d5b51135aa7de95b452a2da70bc19e39b3
Sagor Rana <sagorranait@gmail.com>
2022-10-27 09:54:54 +0600

    Design of SignIn and SignUp page done.

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import { FaGoogle, FaGithubAlt } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import '../styles/SignIn.css';
 
 function SignIn() {
   return (
-    <div>SignIn</div>
+    <section className='signin-area'>
+      <Container>
+        <Row>
+          <Col sm={12} md={12} lg={12} xl={12} xxl={12}>
+            <div className="signin-inputs">
+              <p>Sign in to your account to continue</p>
+              <form className='user-form'>
+                <input type="email" name="email" id="emailInput" placeholder='Email' />
+                <input type="password" name="password" id="passwordInput" placeholder='Password' />
+                <p>Don’t have an account? <Link to='/signup'>Sign Up</Link></p>
+                <button type="submit">Sign In</button>
+              </form>
+              <div className="user-platform">
+                <h4>or</h4>
+                <button><FaGoogle/> Sing in with Google</button>
+                <button><FaGithubAlt/> Sing in with Github</button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import { FaGoogle, FaGithubAlt } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import '../styles/SignIn.css';
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <section className='signin-area'>
+      <Container>
+        <Row>
+          <Col sm={12} md={12} lg={12} xl={12} xxl={12}>
+            <div className="signin-inputs">
+              <p>Please create a new account here</p>
+              <form className='user-form'>
+                <input type="text" name="name" id="nameInput" placeholder='Name' />
+                <input type="url" name="photoUrl" id="urlInput" placeholder='Photo URL' />
+                <input type="email" name="email" id="emailInput" placeholder='Email' />
+                <input type="password" name="password" id="passwordInput" placeholder='Password' />
+                <input type="checkbox" name="agree" id="agree" />
+                <label htmlFor="agree">I've read the <Link>Terms</Link> & <Link>Condition</Link></label>
+                <button type="submit">Sign Up</button>
+              </form>
+              <div className="user-platform">
+                <h4>or</h4>
+                <button><FaGoogle/> Sing up with Google</button>
+                <button><FaGithubAlt/> Sing up with Github</button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
