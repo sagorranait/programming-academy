8f68c5644404e48f1bc019e6e03acadc8f37579c
Sagor Rana <sagorranait@gmail.com>
2022-10-27 22:56:52 +0600

    Blog page questions have been answered.

--- src/components/QuestionAccordion.js
+++ src/components/QuestionAccordion.js
@@ -4,7 +4,7 @@ function QuestionAccordion({eventKey, title, description}) {
   return (
    <Accordion.Item eventKey={eventKey}>
       <Accordion.Header>{title}</Accordion.Header>
-      <Accordion.Body>{description}</Accordion.Body>
+      <Accordion.Body dangerouslySetInnerHTML={{ __html: description }}></Accordion.Body>
    </Accordion.Item>
   )
 }
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -33,22 +33,22 @@ function Blogs() {
                   <QuestionAccordion 
                     eventKey="0" 
                     title="what is cors?"
-                    description="One of the best things about Programming Academy is that you can login to your account from virtually anywhere, whenever you want, and access your courses easily! We strongly believe that students will benefit from the limitless educational possibilities this feature presents. Once you purchase a course, you'll have access for lifetime."
+                    description="Cross-source Resource Sharing (CORS) is an HTTP header-based mechanism that allows a server to indicate any origin (domain, scheme, or port) other than the origin from which the browser should allow download resources. CORS also relies on a mechanism by which the browser makes a preemptive request to the server hosting the cross-origin resource, to verify that the server will allow the actual request. In this preamble, the browser sends headers indicating the HTTP method and the headers to be used in the actual request. Cross-origin request example: UI JavaScript from https: //domain-a.com uses XMLHttpRequest to make requests for https://domain-b.com/data.json."
                   />
                   <QuestionAccordion 
                     eventKey="2" 
                     title="How does the private route work?"
-                    description="We want you to be satisfied, so all eligible courses purchased on Programming Academy can be refunded within 30 days. For whatever reason, if you are unhappy with a course, you can request a refund, provided the request meets the guidelines in our refund policy."
+                    description="Private Routes vary based on the Apps, For example, Dashboard, User Profile, App Settings, Home, etc. In a word, the private route component is similar to the public route, the only changes are the redirect URL and the authentication condition. If the user is not authenticated, he will be redirected to the login page and the user will only be able to access authenticated routes if he is authenticated (logged in)."
                   />
                   <QuestionAccordion 
                     eventKey="3" 
                     title="What is Node? How does Node work?"
-                    description="When you complete a paid Programming Academy course, you will receive a certificate of completion, which you can share with your friends, relatives, co-workers and potential employers!  This article explains how you can download your certificate, after completing your course on a browser."
+                    description="Node.js is an open-source backend javascript runtime environment. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node.js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.<br/> <br/><b>Node.js works:</b> Node.js accepts the request from the clients and sends the response, while working with the request node.js handles them with a single thread. To operate I/O operations or requests node.js use the concept of threads. Thread is a sequence of instructions that the server needs to perform. It runs parallel on the server to provide the information to multiple clients. Node.js is an event loop single-threaded language. It can handle concurrent requests with a single thread without blocking it for one request."
                   />
                   <QuestionAccordion 
                     eventKey="1" 
                     title="Why are you using firebase? What other options do you've to implement authentication?"
-                    description="We want you to be satisfied, so all eligible courses purchased on Programming Academy can be refunded within 30 days. For whatever reason, if you are unhappy with a course, you can request a refund, provided the request meets the guidelines in our refund policy."
+                    description="Because, The Firebase Database lets me build rich, collaborative applications by allowing secure access to the database directly from client-side code. Data is persisted locally, and even while offline, real-time events continue to fire, giving the end user a responsive experience. When the device regains connection, the Realtime Database synchronizes the local data changes with the remote updates that occurred while the client was offline, merging any conflicts automatically.<br/><br/>You can use implement authentication like: <li>Email/Password</li><li>Phone Number</li><li>Google</li><li>Facebook</li><li>Twitter</li><li>Github</li><li>And many more.</li>"
                   />
                 </Accordion>
               </div>
